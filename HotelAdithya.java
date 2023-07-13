6bd48359d70f1e82d515ec0fa9d6f385373b683b
Bhavanagn22 <bhavanagn22@gmail.com>
2023-07-13 14:15:06 +0530

    Array Index added

---
+++ HotelAdithya.java
@@ -0,0 +1,11 @@
+class HotelAdithya{
+   
+   static String foodItems[]={"Shahi Paneer","Paneer Butter Masala","Palak Paneer","Matter Paneer","Paneer Burji","Plain Rice","JeeraRice","Veg Pulao","Paln Roti","Butter Roti Tawa","Tandoori Butter Roti","butter naan","plain naan","lacha paratha","garlic naan","stuff paratha","green paratha","paneer paratha","kadhai chaap","achari chaap","malai chaap","chilli mushrom","chilli paneer","butter milk","gulab jamun","white rasgulla","sujie halwa","fruit custrad","fruit raita","shahi tukra","chatni sandwich","malai kofta","tea","coffee","badam milk","idli sambar","idli vada","vada","dahi idli","veg pakoda","cheese pakoda","dahi vada","sada dosa","masala dosa","rava sada dosa","rava masala dosa","mysore sada","mysore masala","onion rava dosa","paper dosa","upma"};
+   public static void main(String hotel[]){
+   System.out.println(foodItems[0] + " " + foodItems[1] + " " + foodItems[2] + " " + foodItems[3] + " " + foodItems[4] + " " + foodItems[5] + " " + foodItems[6] + " " + foodItems[7] + " " + foodItems[8] + " " + foodItems[9] + " " + foodItems[10] + " " + foodItems[11] + " " + foodItems[12] + " " + foodItems[13] + " " + foodItems[14] + " " + foodItems[15] + " " + foodItems[16] + " " + foodItems[17] + " " + foodItems[18] + " " + foodItems[19] + " " + foodItems[20] + " " + foodItems[21] + " " + foodItems[22] + " " + foodItems[23] + " " + foodItems[24] + " " + foodItems[25] + " " + foodItems[26] + " " + foodItems[27] +" "+ foodItems[28] + " " + foodItems[29] + " " + foodItems[30] + " " + foodItems[31] + " " + foodItems[32] + " " + foodItems[33] + " " + foodItems[34] + " " + foodItems[35] + " " + foodItems[36] + " " + foodItems[37] + " " + foodItems[38] + " " + foodItems[39] + " " + foodItems[40] + " " + foodItems[41] + " " + foodItems[42] + " " + foodItems[43] + " " + foodItems[44] + " " + foodItems[45] + " " + foodItems[46] + " " + foodItems[47] + " " + foodItems[48] + " " + foodItems[49] );
+   
+   }
+   
+   
+}
+  
---
+++ SuperMarketA.java
@@ -0,0 +1,33 @@
+class SuperMarketA{
+
+static String vegetable[] = {"Potato" , "Carrot" , "Beans" , "Brinjal" , "Onion"};
+     static String biscuits[] = {"Orio","ParleG","HideAndSick","Gimjam","Fab","Tiger","Burbon","Bounce","GoodDay","Mary"};
+	 static String perfumes[] = {"Fogg","Yardly","Engage","Max","Signature","Wattagirl","ParkAvenue","Bellavita","Villain","Beardo"};
+	 static String groceries[] = {"Salt","Sugar","CookingOil","Chees","Butter","Yogurt","RedMeat","Hairoil","Pulses","Egg"};
+	 static String cosmetics[] = {"Facecapsule","Skintoner","Facialkit","BleachCream","MakeupBrush","Comb","CurlingTong","FacePack", "FacePuff","FaceWash",HairStraightner",Waxsheet","Trimmer","Haircaps","HairGel","LactoOil","LorialSeries","Lotion","LotusHerbal","MassageCream","Scrub","MacBrush","Waterspray","Shampoo"};
+	 
+	 
+	   public static void main(String market[]){
+	    System.out.println("Main started");
+		System.out.println("=======");
+		
+		System.out.println("List of vegetables are:");
+		System.out.println(vegetable[0] +" "+ vegetable[1] +" "+ vegetable[2] +" "+ vegetable[3] +" "+ vegetable[4] );
+		
+		System.out.println("List of biscuits are:");
+		System.out.println(biscuits[0] +" "+ biscuits[1] +" "+ biscuits[2] +" "+ biscuits[3] +" "+ biscuits[4] +" "+ biscuits[5] +" "+ biscuits[6] +" "+ biscuits[7] +" "+ biscuits[8] +" "+ biscuits[9]  );
+		
+		System.out.println("List of perfumes are:");
+		System.out.println(perfumes[0] +" "+ perfumes[1] +" "+ perfumes[2] +" "+ perfumes[3] +" "+ perfumes[4] +" "+ perfumes[5] +" "+ perfumes[6] +" "+ perfumes[7] +" "+ perfumes[8] +" "+ perfumes[9]  );
+		
+		System.out.println("List of groceries are:");
+		System.out.println(groceries[0] +" "+ groceries[1] +" "+ groceries[2] +" "+ groceries[3] +" "+ groceries[4] +" "+ groceries[5] +" "+ groceries[6] +" "+ groceries[7] +" "+ groceries[8] +" "+ groceries[9]  );
+		
+		
+		System.out.println("List of cosmetics are:");
+		System.out.println(cosmetics[0] +" "+ cosmetics[1] +" "+ cosmetics[2] +" "+ cosmetics[3] +" "+ cosmetics[4] +" "+cosmetics[5] +" "+cosmetics[6] +" "+cosmetics[7] +" "+ cosmetics[8] +" "+ cosmetics[9] +" "+ cosmetics[10] +" "+ cosmetics[11] +" "+ cosmetics[12] +" "+ cosmetics[13] +" "+ cosmetics[14]+" "+ cosmetics[15] +" "+ cosmetics[16] +" "+ cosmetics[17] +" "+ cosmetics[18] +" "+ cosmetics[19] +" "+ cosmetics[20] +" "+ cosmetics[21] +" "+ cosmetics[22] +" "+ cosmetics[23] +" "+ cosmetics[24] );
+		
+		}
+
+
+}
